68bd551266c8f3ba39e9020f5110e285b9acbee2
Mac Meldi Roch <mmeldiroch@gmail.com>
2021-10-07 11:41:50 -0300

    Creacion del esqueleto del navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import './navbar.css';
+
+export const Navbar = () => {
+    return <nav className="App-navbar">
+                <div className="App-navbar-brand">
+                    <h2>MC Training</h2>
+                    <h1>- Entrenamientos Personalizados -</h1>
+                </div>
+                <ul className="App-navbar-menu">
+                    <li><a href="">Clases</a></li>
+                    <li><a href="">Planes de entrenamiento</a></li>
+                    <li><a href="">Carrito</a></li>
+                </ul>
+             </nav>
+}
