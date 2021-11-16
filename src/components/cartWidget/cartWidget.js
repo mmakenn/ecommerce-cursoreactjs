b1a6f3725d0430d40223b7ea4c018efca4ff44fb
Mac Meldi Roch <mmeldiroch@gmail.com>
2021-11-16 16:30:45 -0300

    Carrito de compras funcionando. Se corrigio el render del contador del cartwidget

--- src/components/cartWidget/cartWidget.js
+++ src/components/cartWidget/cartWidget.js
@@ -2,21 +2,14 @@ import './cartWidget.css';
 import cartWidget from './cart.png';
 import { Container } from 'react-bootstrap';
 import { useCartContext } from '../../context/cartContext';
-import { useEffect, useState } from 'react';
 
 export const CartWidget = () => {
-    const cart = useCartContext();
-    const [quantity, setQuantity] = useState(0);
-
-    useEffect(() => {
-        setQuantity(cart.getTotalQuantity());
-    }
-    , [cart]);
+    const cart = useCartContext();    
 
     return (
         <Container className='d-flex'>
             <img src={cartWidget} alt='Cart Widget'/>
-            <p>( {quantity} )</p>
+            <p>( {cart.itemsQuantity} )</p>
         </Container>
     );
 }
--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -6,6 +6,7 @@ export const useCartContext = () => useContext(CartContext);
 
 export const CartProvider = ( {children} ) => {
     const [items, setItems] = useState([]);
+    const [itemsQuantity, setItemsQuantity] = useState(0);
 
     const addProduct = (product, quantity) => {
         const foundedProduct = items.find(p => p.id === product.id);
@@ -15,12 +16,15 @@ export const CartProvider = ( {children} ) => {
             product.quantity = quantity;
             setItems([...items, product]);
         }
+        setItemsQuantity(itemsQuantity + quantity);
     }
 
     const removeProduct = (product) => {
         setItems(items.filter((prodInCart) => {
             return prodInCart.id !== product.id;
         }));
+
+        setItemsQuantity(itemsQuantity - product.quantity);
     }
 
     const clearCart = () => {
@@ -33,14 +37,8 @@ export const CartProvider = ( {children} ) => {
         return total;
     }
 
-    const getTotalQuantity = () => {
-        let total = 0
-        items.forEach( item => total = total + item.quantity );
-        return total;
-    }
-
     return (
-        <CartContext.Provider value={{ items, addProduct, removeProduct, clearCart, getTotalPrice, getTotalQuantity }}>
+        <CartContext.Provider value={{ items, itemsQuantity, addProduct, removeProduct, clearCart, getTotalPrice }}>
             { children }
         </CartContext.Provider>
     );
