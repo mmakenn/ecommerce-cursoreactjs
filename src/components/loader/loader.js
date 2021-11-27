fdc705b4c9bcb3cd345b3352148c65d84d1f4db8
Mac Meldi Roch <mmeldiroch@gmail.com>
2021-11-26 23:11:53 -0300

    Se eliminó una condición que impedía que se renderice el loader

--- src/components/loader/loader.js
+++ src/components/loader/loader.js
@@ -1,11 +1,10 @@
-import { Spinner } from "react-bootstrap";
+import { Container, Spinner } from "react-bootstrap";
 
-export const Loader = (props) => {
-    if (!props.status) return <> </>
+export const Loader = () => {
     return (
-        <>
+        <Container className="w-100 mt-5 d-flex flex-column justify-content-center align-items-center">
         <Spinner animation="border" variant="warning" />
         <p>Cargando...</p>
-        </>
+        </Container>
     );
 }
