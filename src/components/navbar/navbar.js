25af80e2d658db25db2db457e0343b7f9ec51dfd
Mac Meldi Roch <mmeldiroch@gmail.com>
2021-11-28 18:12:11 -0300

    Se crearon categorias dinamicas a partir de la base de datos

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,8 +1,22 @@
 import { Link } from 'react-router-dom';
 import './navbar.css';
 import { CartWidget } from '../cartWidget/cartWidget.js';
+import { useEffect, useState } from 'react';
+
+import { db } from '../../firebase';
+import { collection, getDocs } from '@firebase/firestore';
 
 export const Navbar = () => {
+    const [categories, setCategories] = useState(null);
+
+    useEffect(() => {
+        const loadedCategories = collection(db, 'navbar');
+        getDocs(loadedCategories)
+        .then((snapshot) => {
+        setCategories(snapshot.docs.map((doc) => {return {...doc.data(), id: doc.id}}));
+        })
+    }, []);
+
     return (
         <nav className="App-navbar">
             <div className="App-navbar-brand">
@@ -11,8 +25,13 @@ export const Navbar = () => {
             </div>
             <ul className="App-navbar-menu">
                 <li><Link to={`/`}> Home </Link></li>
-                <li><Link to={`/category/clases`}> Clases </Link></li>
-                <li><Link to={`/category/planes`}> Planes de entrenamiento </Link> </li>
+                { categories && categories.map(category => 
+                                        <li key={category.id}>
+                                            <Link to={`/category/${category.name}`}>
+                                                {category.name}
+                                            </Link>
+                                        </li>
+                ) }
                 <li><Link to={`/carrito`}> Carrito <CartWidget/></Link></li>
             </ul>
         </nav>
