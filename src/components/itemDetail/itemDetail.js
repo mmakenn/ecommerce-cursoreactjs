b085adea42bc60b620cfda4b6c170ac69afba5ac
Mac Meldi Roch <mmeldiroch@gmail.com>
2021-11-28 17:54:41 -0300

    Se agrego el stock en el detalle del producto y se modifico la estetica de las cards

--- src/components/itemDetail/itemDetail.js
+++ src/components/itemDetail/itemDetail.js
@@ -1,4 +1,4 @@
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { ItemCount } from "../itemCount/itemCount";
 import { useCartContext } from "../../context/cartContext";
@@ -16,27 +16,31 @@ export const ItemDetail = (props) => {
     } 
 
     return (
-        <Card style={{ width: '50rem' }} className='m-auto'>
-            <Card.Header className='d-flex flex-column'>
+        <Card className='w-75 m-5'>
+            <Card.Header>
                 <Card.Text className='h2'>{product.title}</Card.Text>
-                <Card.Img variant="top" src={product.img} className='w-50' alt={product.description}/>
             </Card.Header>
             <Card.Body>
-                <Card.Text>
+                <Card.Img src={product.img} className='w-50 m-3' alt={product.description}/>
+                <Card.Text className='h4'>
                     {product.description}
                 </Card.Text>
-                <Card.Text>
+                <Card.Text className='h5'>
                     {product.largeDescription}
                 </Card.Text>
-                <Card.Text>
+                <Card.Text className='h4'>
                     $ {product.price}
+                </Card.Text>
+                <Card.Text className="text-muted">
+                    Stock disponible: {product.stock}
                 </Card.Text>
                 <ItemCount stock={product.stock} initial={getInitialCountValue} addToCart={addToCart}/>
                 <Button variant='warning' className='m-3'><Link to='/'> Volver </Link></Button>
                 <Button variant='warning' className='m-3'><Link to='/carrito'> Ver Carrito </Link></Button>
             </Card.Body>
-            <Card.Footer className="text-muted">Una vez acreditado el pago, se enviara un mail con un formulario
-             para que te contactemos dentro de las 48 hs.</Card.Footer>
+            <Card.Footer className="text-muted">
+                Una vez acreditado el pago, te contactemos dentro de las 48 hs.
+            </Card.Footer>
         </Card>
     );  
 }
