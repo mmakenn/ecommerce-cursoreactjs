2045787ae93d48f513db319d014fdc6efe7d6907
Mac Meldi Roch <mmeldiroch@gmail.com>
2021-11-29 20:56:37 -0300

    Se agrega page 404 para rutas por fuera de las determinadas en el flow

--- src/components/pageNotFound/pageNotFound.js
+++ src/components/pageNotFound/pageNotFound.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { Container, Button } from "react-bootstrap";
 
 export const PageNotFound = () => {
+    /* Renderiza mensaje de error 404 para rutas que no están determinadas en el flow de la app. */
     return (
         <Container className='w-50 d-flex flex-column justify-content-center'>
             <p className='h1 m-3'>
